f02a60354cc3c0cead504fe7cc932b347938248c
kiman <kim.andre.ottesen@gmail.com>
2017-11-09 15:33:51 +0100

    Removed svg filter, add movement tracking script

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -102,7 +102,7 @@
 			cacheLength: 4,
 			loadingClass: 'is-loading',
 			onStart: {
-				duration: 750,
+				duration: 0,
 				render: function ( $container ) {
 					$container.addClass('glitch');
 				}
@@ -118,6 +118,158 @@
 		};
 
 		$('#container').smoothState( settings );
+	});
+
+	// Draw
+	var canvas = $('#c'),
+		viewportW = $(window).width(),
+		viewportH = $(window).height();
+		docH = $(document).height();
+		// console.log('Total document height: ' + docH + ' px');
+	$(canvas).attr('width', viewportW);
+	$(canvas).attr('height', docH);
+	$(canvas).addClass('active');
+
+	// Get a regular interval for drawing to the screen
+	window.requestAnimFrame = (function (callback) {
+		return window.requestAnimationFrame || 
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimaitonFrame ||
+			function (callback) {
+			 	window.setTimeout(callback, 1000/60);
+			};
+	})();
+
+	var initX;
+	var initY;
+
+	$(document).mousemove(function(event){
+		initX = event.pageX;
+		initY = event.pageY;
+		return initX;
+		return initY;
+	});
+
+	// Set up the canvas
+	var canvas = document.getElementById("c");
+	var body = document.body;
+	var ctx = canvas.getContext("2d");
+	ctx.strokeStyle = "#000";
+	ctx.font = "bold 16px Arial";
+	ctx.lineWidth = 0.25;
+	ctx.translate(0.5, 0.5);
+	ctx.lineJoin = ctx.lineCap = 'round';
+
+
+	// Set up mouse events for drawing
+	var drawing = true;
+	var mousePos = { x:initX, y:initY };
+	var lastPos = mousePos;
+
+	body.addEventListener("mousedown", function (e) {
+		drawing = true;
+		lastPos = getMousePos(canvas, e);
+	}, false);
+	body.addEventListener("mouseup", function (e) {
+		drawing = true;
+	}, false);
+	body.addEventListener("mousemove", function (e) {
+		mousePos = getMousePos(canvas, e);
+	}, false);
+
+	// Set up touch events for mobile, etc
+	canvas.addEventListener("touchstart", function (e) {
+		mousePos = getTouchPos(canvas, e);
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousedown", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+	canvas.addEventListener("touchend", function (e) {
+		var mouseEvent = new MouseEvent("mouseup", {});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+	canvas.addEventListener("touchmove", function (e) {
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousemove", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+
+	// Prevent scrolling when touching the canvas
+	document.body.addEventListener("touchstart", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
+	document.body.addEventListener("touchend", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
+	document.body.addEventListener("touchmove", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
+
+	// Get the position of the mouse relative to the canvas
+	function getMousePos(canvasDom, mouseEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: mouseEvent.clientX - rect.left,
+			y: mouseEvent.clientY - rect.top
+		};
+	}
+
+	// Get the position of a touch relative to the canvas
+	function getTouchPos(canvasDom, touchEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: touchEvent.touches[0].clientX - rect.left,
+			y: touchEvent.touches[0].clientY - rect.top
+		};
+	}
+
+	// Draw to the canvas
+	function renderCanvas() {
+		if (drawing) {
+			ctx.moveTo(lastPos.x, lastPos.y);
+			ctx.lineTo(mousePos.x, mousePos.y);
+			ctx.stroke();
+			lastPos = mousePos;
+		}
+	}
+
+	function clearCanvas() {
+		canvas.width = canvas.width;
+		ctx.lineWidth = 0.01;
+	}
+
+	// Allow for animation
+	(function drawLoop () {
+		requestAnimFrame(drawLoop);
+		renderCanvas();
+	})();
+
+	$(window).on('beforeunload', function(){
+		var dataURL = canvas.toDataURL();
+		console.log(dataURL);
+	    $.ajax({
+			type: "POST",
+			url: "upload.php",
+			data: { 
+			 imgBase64: dataURL
+			}
+		}).done(function(o) {
+			console.log('Saved'); 
+		});
 	});
 
 })(jQuery);
